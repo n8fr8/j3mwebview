f4e994ecdf7ce50d1dcc122a939460225ab02f2d
n8fr8 <nathan@freitas.net>
2013-10-10 11:06:57 -0400

    actually parsing J3M now!

--- js/j3mweb.js
+++ js/j3mweb.js
@@ -1,14 +1,101 @@
 
 $(function () {
   
+    loadMedia("../sample/sample.jpg");
+
+    parseJ3M("../sample/samplej3m.json");
+
+});
+
+function parseJ3M (path)
+{
+
+  $.getJSON(path, function(j3m) {
+    
+    $('#content_title').text(j3m.genealogy.hashes[0])
+
+    //$('#creator_fingerprint').text(jemtest.intent.pgpKeyFingerprint);
+  
+ //  $('#destination').text(jemtest.intent.intendedDestination);
+    //$('#timestamp').text(jemtest.data.exif.timestamp);
+
+  $.each(j3m.intent,function(id, item){
+        $("#intent_content").append("<div>" + id + ": " + item + "</div>");
+    });
+
+  $.each(j3m.data.exif,function(id, item){
+        $("#camera_content").append("<div>" + id + ": " + item + "</div>");
+    });
+
+  $.each(j3m.genealogy,function(id, item){
+        $("#gene_content").append("<div>" + id + ": " + item + "</div>");
+    });
+
+
+  $.each(j3m.data.userAppendedData,function(id, item){
+
+
+      $.each(item,function(id, item){
+
+        if(typeof item =='object')
+        {
+             $.each(item,function(id, item){
+                 $("#user_content").append("<div>" + id + ": " + item + "</div>");
+     
+             });
+        }
+        else
+          $("#user_content").append("<div>" + id + ": " + item + "</div>");
+
+      });
+
+    });
+
+
+$.each(j3m.data.sensorCapture,function(id, item){
+
+      $.each(item,function(id, item){
+
+
+        if(typeof item =='object')
+        {
+             $.each(item,function(id, item){
+                 $("#sensor_content").append("<div>" + id + ": " + item + "</div>");
+     
+             });
+        }
+        else
+          $("#sensor_content").append("<div>" + id + ": " + item + "</div>");
+
+      });
+
+    });
+
+
+
+  })
+
+}
+
+function loadMedia (path)
+{
+
+  $("#img-main").attr("src",path);
+
+
+  //  setupMediaMask(1200,900);
+
+
+}
+
+function setupMediaMask (imgWidth, imgHeight)
+{
 
   $("#img-main").css({top: 0, left: 0});
 
 var maskWidth  = $("#img-mask").width();
 var maskHeight = $("#img-mask").height();
 var imgPos     = $("#img-main").offset();
-var imgWidth   = $("#img-main").width();
-var imgHeight  = $("#img-main").height();
 
 var x1 = (imgPos.left + maskWidth) - imgWidth;
 var y1 = (imgPos.top + maskHeight) - imgHeight;
@@ -18,22 +105,4 @@ var y2 = imgPos.top;
 $("#img-main").draggable({ containment: [x1,y1,x2,y2] });
 $("#img-main").css({cursor: 'move'});
 
-  //$('#creator').text(jemtest.intent.alias);
-  //$('#creator_fingerprint').text(jemtest.intent.pgpKeyFingerprint);
-  
- //  $('#destination').text(jemtest.intent.intendedDestination);
-  
-  /**
-  * "genealogy": {
-        "localMediaPath": "/cdbd82faa427cd6ee515ee80e9833f1733f84e34",
-        "dateCreated": 1380650827093,
-        "hashes": [
-            "fde4fe74d21d40d3d56ff52da74e92b0"
-        ],
-        "createdOnDevice": "f9eb211123fc2415917f5cad7ab61ca6c73caad0"
-    }
-  */
-  
-  //$('#timestamp').text(jemtest.data.exif.timestamp);
-  
-});
+}
